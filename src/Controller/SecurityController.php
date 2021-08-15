412edb278c0b8484e9e64c4acdb867bf76898c83
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-08-15 15:19:47 +0200

    Add security controller for registration

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hash){
+        $user = new User;
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() AND $form->isValid()){
+            $hash = $hash->hashPassword($user, $user->getPassword());
+
+            $user->setPassword($hash);
+            
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
