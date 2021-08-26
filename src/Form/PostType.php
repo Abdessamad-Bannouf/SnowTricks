4460a89c89949c90bb0b8854b6d554a16f1f7dc5
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-08-26 19:23:28 +0200

    Add post type form for post add

---
+++ src/Form/PostType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Titre de l'article"
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => "Description de l'article",
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('photo', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Image de l'article",
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('video', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Vidéo de l'article",
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('group', EntityType::class, [
+                'class' => Group::class,
+                'choice_label' => 'name',
+                'multiple' => false
+            ])
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
