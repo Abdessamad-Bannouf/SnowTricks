0ef50a743b8815cb8f2fbc0696de1cf3de631f8b
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-09-09 20:42:27 +0200

    Add TextType for video

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Video;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class VideoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, ['attr' => [
+                'placeholder' => "Titre de l'article"
+            ]])
             ->add('post')
         ;
     }
