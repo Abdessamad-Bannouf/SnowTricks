9d7888fd24317986b324a15997b5788cffce58f5
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-09-30 21:02:09 +0200

    Fix the merge errors

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -109,11 +109,6 @@ class Post
         return $this;
     }
 
-    public function getVideos(): ?string
-    {
-        return $this->videos;
-    }
-
     public function setVideos(string $videos): self
     {
         $this->videos = $videos;
@@ -146,43 +141,6 @@ class Post
     }
 
     /**
-     * @return Collection|Comment[]
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setPost($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getPost() === $this) {
-                $comment->setPost(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function __toString(){
-        // to show the name of the Category in the select
-        return $this->name;
-        // to show the id of the Category in the select
-        // return $this->id;
-    }
-
-    /**
      * @return Collection|Photo[]
      */
     public function getPhotos(): Collection
@@ -202,7 +160,6 @@ class Post
 
     public function removePhoto(Photo $photo): self
     {
-        dump($this->photos->contains($photo));die();
         if ($this->photos->removeElement($photo)) {
             // set the owning side to null (unless already changed)
             if ($photo->getPost() === $this) {
@@ -270,12 +227,5 @@ class Post
         }
 
         return $this;
-    }
-
-    public function __toString(){
-        // to show the name of the Category in the select
-        return $this->name;
-        // to show the id of the Category in the select
-        // return $this->id;
     }
 }
