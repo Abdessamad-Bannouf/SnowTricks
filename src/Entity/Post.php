47e42b918ff6ccffc9ce8e66c5915a37cb5b1bf4
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-09-30 21:13:03 +0200

    Fix the merge errors

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -109,11 +109,6 @@ class Post
         return $this;
     }
 
-    public function getVideos(): ?string
-    {
-        return $this->videos;
-    }
-
     public function setVideos(string $videos): self
     {
         $this->videos = $videos;
@@ -143,43 +138,6 @@ class Post
         $this->group = $group;
 
         return $this;
-    }
-
-    /**
-     * @return Collection|Comment[]
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setPost($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getPost() === $this) {
-                $comment->setPost(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function __toString(){
-        // to show the name of the Category in the select
-        return $this->name;
-        // to show the id of the Category in the select
-        // return $this->id;
     }
 
     /**
@@ -239,6 +197,7 @@ class Post
                 $video->setPost(null);
             }
         }
+    }
 
     /**
      * @return Collection|Comment[]
@@ -268,12 +227,5 @@ class Post
         }
 
         return $this;
-    }
-
-    public function __toString(){
-        // to show the name of the Category in the select
-        return $this->name;
-        // to show the id of the Category in the select
-        // return $this->id;
     }
 }
