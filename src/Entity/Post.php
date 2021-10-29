e34ce2642009de4759b6a90cec6019dc3a2812c5
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-10-29 15:01:20 +0200

    Put unique entity for slug

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -7,9 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\JoinColumn;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
+ * @UniqueEntity(fields={"name"})
  */
 class Post
 {
