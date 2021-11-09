37e2e0e3b2bc56dcd1810734b55b280f91662c70
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-11-09 19:14:31 +0100

    Fix the merging error (add the method login)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,6 +16,23 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+    
+    /**
      * @Route("/inscription", name="security_registration")
      */
     public function registration(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hash){
