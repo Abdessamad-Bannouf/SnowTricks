8d68852a541a5985213605377d067c62bd4a8b42
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-12-03 21:25:56 +0100

    Fix unmerged files

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,31 +14,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
-
-    /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout()
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
     /**
      * @Route("/inscription", name="security_registration")
      */
@@ -54,7 +28,7 @@ class SecurityController extends AbstractController
             $hash = $hash->hashPassword($user, $user->getPassword());
 
             $user->setPassword($hash);
-
+            
             $manager->persist($user);
             $manager->flush();
         }
