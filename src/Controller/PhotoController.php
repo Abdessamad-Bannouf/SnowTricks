616f7c66562623e531f86e63800cd93fe7ecb8c6
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-12-16 18:05:06 +0100

    Generate photo controller

---
+++ src/Controller/PhotoController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Photo;
+use App\Form\PhotoType;
+use App\Repository\PhotoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/photo")
+ */
+class PhotoController extends AbstractController
+{
+    /**
+     * @Route("/", name="photo_index", methods={"GET"})
+     */
+    public function index(PhotoRepository $photoRepository): Response
+    {
+        return $this->render('photo/index.html.twig', [
+            'photos' => $photoRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="photo_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $photo = new Photo();
+        $form = $this->createForm(PhotoType::class, $photo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($photo);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('photo_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('photo/new.html.twig', [
+            'photo' => $photo,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="photo_show", methods={"GET"})
+     */
+    public function show(Photo $photo): Response
+    {
+        return $this->render('photo/show.html.twig', [
+            'photo' => $photo,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="photo_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Photo $photo): Response
+    {
+        //$file = new File($this->getParameter('images_directory').'/'.$photo->getName());
+
+        /*$file = $photo->getName();
+        $photo->setName($file);
+
+        if ($file instanceof UploadedFile) {
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move(
+               $this->getParameter('profile.picture.attachment.dir'),
+               $fileName
+           );
+           die("test");
+           $photo->setPicture($fileName);
+           //$em->persist($photo); // Place here
+       }*/
+        $form = $this->createForm(PhotoType::class, $photo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('photo_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('photo/edit.html.twig', [
+            'photo' => $photo,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="photo_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Photo $photo): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$photo->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($photo);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('photo_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
