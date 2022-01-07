7fc77ee0a225db8c2ae612c42ec7978ccc2d087d
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2022-01-07 15:33:23 +0100

    add authentication restriction for crud post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -40,83 +40,89 @@ class PostController extends AbstractController
      */
     public function new(Request $request): Response
     {
-        $post = new Post();
-
-        $entityManager = $this->getDoctrine()->getManager();
-        
-        $form = $this->createForm(PostType::class, $post);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // On récupère les images transmises
-            $images = $form->get('photos')->getData();
-
-            // On récupère les vidéos transmises
-            $videos = $form->get('videos')->getData();
-
-            // On boucle sur les images
-            foreach($images as $image){
-                $image = new File($image->getName());
-                
-                // On génère un nouveau nom de fichier
-                $fichier = md5(uniqid()) . '.' . $image->guessExtension();
-
-                // On copie le fichier dans le dossier upload
-                $image->move(
-                    $this->getParameter('images_directory'),
-                    $fichier);
-
-                // On stocke l'image dans la base de données (son nom)
-                $photo = new Photo;
-                $photo->setName($fichier);
-                $post->addPhoto($photo);
-            }
-
-
-
-            // On boucle sur les images
-            foreach($videos as $link){
-                // On stocke la vidéo dans la base de données (son nom)
-                //$video = new Video;
-                $link->setName($link->getName());
-                $post->addVideo($link);
-            }
-
-
-            // On récupère l'image principale qui va servir pour la page pour afficher la liste
-            $mainImage = $form->get('photo')->getData();
-            $fichier = md5(uniqid()) . '.' . $mainImage->guessExtension();
-
-            // On copie le fichier dans le dossier upload
-            $mainImage->move(
-                $this->getParameter('images_directory'),
-                $fichier);
-            $mainImage = $post->setPhoto($fichier);
-
-            // On instancie la date
-            $date = new \DateTime();
-            $date = $post->setDate($date);
-
-            //On persiste le nom sluggé dans la colonne slug
-            $slugger = new AsciiSlugger('fr', ['fr' => [' ' => '-', 'à' => 'a', 'â' => 'a', 'é' => 'e', 'è' => 'e', 'ê' => 'e', 'î' => 'i', 'ï' => 'i', 'ô' => 'o', 'û' => 'u']]);
-            $post->setSlug($slugger->slug($post->getName()));
+        // Si l'utilisateur est connecté il peut modifier un trick
+        if($this->getUser())
+        {
+            $post = new Post();
 
             $entityManager = $this->getDoctrine()->getManager();
-          
-            // On stocke l'image principale dans la base de données (son nom)
-            //$entityManager->persist($mainImage);
-            // On stocke la date dans la base de données
-            //$entityManager->persist($date);
-            $entityManager->persist($post);
             
-            $entityManager->flush();
+            $form = $this->createForm(PostType::class, $post);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                // On récupère les images transmises
+                $images = $form->get('photos')->getData();
 
-            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+                // On récupère les vidéos transmises
+                $videos = $form->get('videos')->getData();
+
+                // On boucle sur les images
+                foreach($images as $image){
+                    $image = new File($image->getName());
+                    
+                    // On génère un nouveau nom de fichier
+                    $fichier = md5(uniqid()) . '.' . $image->guessExtension();
+
+                    // On copie le fichier dans le dossier upload
+                    $image->move(
+                        $this->getParameter('images_directory'),
+                        $fichier);
+
+                    // On stocke l'image dans la base de données (son nom)
+                    $photo = new Photo;
+                    $photo->setName($fichier);
+                    $post->addPhoto($photo);
+                }
+
+
+
+                // On boucle sur les images
+                foreach($videos as $link){
+                    // On stocke la vidéo dans la base de données (son nom)
+                    //$video = new Video;
+                    $link->setName($link->getName());
+                    $post->addVideo($link);
+                }
+
+
+                // On récupère l'image principale qui va servir pour la page pour afficher la liste
+                $mainImage = $form->get('photo')->getData();
+                $fichier = md5(uniqid()) . '.' . $mainImage->guessExtension();
+
+                // On copie le fichier dans le dossier upload
+                $mainImage->move(
+                    $this->getParameter('images_directory'),
+                    $fichier);
+                $mainImage = $post->setPhoto($fichier);
+
+                // On instancie la date
+                $date = new \DateTime();
+                $date = $post->setDate($date);
+
+                //On persiste le nom sluggé dans la colonne slug
+                $slugger = new AsciiSlugger('fr', ['fr' => [' ' => '-', 'à' => 'a', 'â' => 'a', 'é' => 'e', 'è' => 'e', 'ê' => 'e', 'î' => 'i', 'ï' => 'i', 'ô' => 'o', 'û' => 'u']]);
+                $post->setSlug($slugger->slug($post->getName()));
+
+                $entityManager = $this->getDoctrine()->getManager();
+            
+                // On stocke l'image principale dans la base de données (son nom)
+                //$entityManager->persist($mainImage);
+                // On stocke la date dans la base de données
+                //$entityManager->persist($date);
+                $entityManager->persist($post);
+                
+                $entityManager->flush();
+
+                return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('post/new.html.twig', [
+                'post' => $post,
+                'form' => $form->createView()
+            ]);     
         }
-
-        return $this->render('post/new.html.twig', [
-            'post' => $post,
-            'form' => $form->createView()
-        ]);
+        
+        return $this->render('post/404.html.twig', []);
     }
 
     /**
@@ -124,22 +130,27 @@ class PostController extends AbstractController
      */
     public function edit(Request $request, Post $post): Response
     {
-        $form = $this->createForm(PostType::class, $post);
-        $form->handleRequest($request);
+        // Si l'utilisateur est connecté il peut modifier un trick
+        if($this->getUser()) {
+            $form = $this->createForm(PostType::class, $post);
+            $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $slugger = new AsciiSlugger('fr', ['fr' => [' ' => '-', 'à' => 'a', 'â' => 'a', 'é' => 'e', 'è' => 'e', 'ê' => 'e', 'î' => 'i', 'ï' => 'i', 'ô' => 'o', 'û' => 'u']]);
-            $post->setSlug($slugger->slug($post->getName()));
+            if ($form->isSubmitted() && $form->isValid()) {
+                $slugger = new AsciiSlugger('fr', ['fr' => [' ' => '-', 'à' => 'a', 'â' => 'a', 'é' => 'e', 'è' => 'e', 'ê' => 'e', 'î' => 'i', 'ï' => 'i', 'ô' => 'o', 'û' => 'u']]);
+                $post->setSlug($slugger->slug($post->getName()));
 
-            $this->getDoctrine()->getManager()->flush();
+                $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+                return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('post/edit.html.twig', [
+                'post' => $post,
+                'form' => $form->createView()
+            ]);
         }
 
-        return $this->render('post/edit.html.twig', [
-            'post' => $post,
-            'form' => $form->createView()
-        ]);
+        return $this->render('post/404.html.twig', []);
     }
 
     /**
