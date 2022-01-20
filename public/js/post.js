6321f1ede1681c42df6fe52aed5213e9ba5610a8
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2022-01-20 17:59:51 +0100

    Add icons in home

--- public/js/post.js
+++ public/js/post.js
@@ -2,3 +2,8 @@ function seeMore() {
     var load = document.getElementsByClassName("container-media")[0];
     load.style.display = "flex";
 }
+
+function seeMorePosts() {
+    var load = document.getElementsByClassName("row")[0];
+    load.style.display = "flex";
+}
